bcc00549f44b0dbe5a5825cd47dde8621b06a6b8
Alamgir kabir <alamgirkabir.dev@gmail.com>
2022-12-16 21:33:15 +0600

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 55556;
+const singers = [
+  {name: 'Dr.Mahfuz', job: 'Singer'},
+ {name: 'Eva Rahman', job: 'singer'},
+ {name: 'Agun', job: 'Sopno'},
+ {name: 'shovro', job: 'phathor'}
+]
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'white',
+}
+
 function App() {
+  const nayoks =['Rubel', 'Babaraj', 'kober', ' Jashim', ' salman shah','Riyaz'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayoks.map(nayok => <li>Name: {nayok}</li>)
+      }
+      {/* {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      } */}
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
+      {/* <Person name={[0]} nayika="moushomi"></Person>
+      <Person name={[1]} nayika="cheka"></Person>
+      <Person name={[2]} nayika="kopila"></Person> */}
+     
+      <h5>New component. YAY</h5>
+      <h4>Rock mama. React mama</h4>
+      <Friend movie="shingam" phone="01777"></Friend>
+      <Friend phone="019999"></Friend>
     </div>
   );
+}
+
+function Person (props){
+  return (
+    <div className='person'>
+      <h1>{props.name}</h1>
+      <p></p>
+    </div>
+  ) 
+}
+  function Friend (props){
+    console.log(props);
+    return (
+     <div className='container'>
+       <h3>Name:{props.movie}</h3>
+        <p>phone:{props.phone} </p>
+     </div>
+  )
 }
 
 export default App;
